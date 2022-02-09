b6b76f51c4481e0612af1d89b6991b6e6e6c4ac6
Marvin Wright <github@wrig.de>
2022-02-09 08:36:56 +0100

    update R6 version with data.table version

--- genrf/R/genrf.R
+++ genrf/R/genrf.R
@@ -7,29 +7,29 @@
 #' Details here
 #' @importFrom R6 R6Class
 #' @importFrom foreach foreach %dopar%
+#' @importFrom data.table data.table melt setDT
 #' @export
 genrf <- R6::R6Class(
   classname = "genrf",
   public = list(
+    # Create and fit a generative random forest
     initialize = function(x, oob = FALSE, dist = "normal", ...) {
       # Convert input to data.frame
       private$orig_colnames <- colnames(x)
       x_real <- data.frame(x)
       private$p <- ncol(x_real)
-      private$levels <- lapply(x_real, levels)
       private$dist <- dist
 
       # Convert chars and logicals to factors
       private$idx_char <- sapply(x_real, is.character)
       if (any(private$idx_char)) {
-        x_real[, private$idx_char] <- as.data.frame(lapply(x_real[, private$idx_char], as.factor))
+        x_real[, private$idx_char] <- as.data.frame(lapply(x_real[, private$idx_char, drop = FALSE], as.factor))
       }
       private$idx_logical <- sapply(x_real, is.logical)
       if (any(private$idx_logical)) {
-        x_real[, private$idx_logical] <- as.data.frame(lapply(x_real[, private$idx_logical], as.factor))
+        x_real[, private$idx_logical] <- as.data.frame(lapply(x_real[, private$idx_logical, drop = FALSE], as.factor))
       }
       private$factor_cols <- sapply(x_real, is.factor)
-      #factor_col_names <- names(factor_cols)[factor_cols]
 
       # Sample from marginals to get naive synthetic data
       x_synth <- as.data.frame(lapply(x_real, function(x) {
@@ -54,73 +54,58 @@ genrf <- R6::R6Class(
       }
 
       # Get probabilities of terminal nodes for each tree
-      # probs dims: [nodeid, tree]
-      private$probs <- apply(pred, 2, function(x) {
+      private$node_probs <- apply(pred, 2, function(x) {
         tab <- tabulate(x, nbins = max(pred, na.rm = TRUE))
         tab[tab == 1] <- 0 # Avoid terminal nodes with just one obs
         tab/sum(tab)
       })
 
-      # Fit continuous distribution in all used terminal nodes
-      # params dims: [[tree]][[nodeid]][[colname]][distr. parameters]
+      # Fit continuous distribution in all terminal nodes
       if (any(!private$factor_cols)) {
-        private$params <- foreach(tree=1:private$num_trees) %dopar% {
-          unique_nodeids <- which(private$probs[, tree] > 0)
+        private$params <- foreach(tree = 1:private$num_trees, .combine = rbind) %dopar% {
+          dt <- data.table(tree = tree, x_real[, !private$factor_cols, drop = FALSE], nodeid = pred[, tree])
+          long <- melt(dt, id.vars = c("tree", "nodeid"))
+
           if (dist == "normal") {
-            # Use faster analytical version for normal distribution
-            res <- lapply(unique_nodeids, function(nodeid) {
-              idx <- which(pred[, tree] == nodeid)
-              sapply(x_real[idx, !private$factor_cols, drop = FALSE], function(x) {
-                c(mean = mean(x), sd = sd(x)) # Or use MLE (1/n)?
-              })
-            })
+            long[, list(mean = mean(value), sd = sd(value)), by = .(tree, nodeid, variable)]
           } else {
-            res <- lapply(unique_nodeids, function(nodeid) {
-              idx <- which(pred[, tree] == nodeid)
-              apply(x_real[idx, !private$factor_cols, drop = FALSE], 2, function(x) {
-                MASS::fitdistr(x, dist)$estimate
-              })
-            })
+            long[, as.list(MASS::fitdistr(value, dist)$estimate), by = .(tree, nodeid, variable)]
           }
-          names(res) <- unique_nodeids
-          res
         }
       }
 
-      # Calculate class probabilities for categorical data in all used terminal nodes
-      # class_probs dims: [[tree]][[nodeid]][[colname]][class probs]
+      # Calculate class probabilities for categorical data in all terminal nodes
       if (any(private$factor_cols)) {
-        private$class_probs <- foreach(tree=1:private$num_trees) %dopar% {
-          unique_nodeids <- which(private$probs[, tree] > 0)
-          res <- lapply(unique_nodeids, function(nodeid) {
-            idx <- which(pred[, tree] == nodeid)
-            lapply(x_real[idx, private$factor_cols, drop = FALSE], function(x) {
-              tabulate(x, nbins = nlevels(x))
-            })
-          })
-          names(res) <- unique_nodeids
-          res
+        private$class_probs <- foreach(tree = 1:private$num_trees, .combine = rbind) %dopar% {
+          dt <- data.table(tree = tree, x_real[, private$factor_cols, drop = FALSE], nodeid = pred[, tree])
+          long <- melt(dt, id.vars = c("tree", "nodeid"), value.factor = TRUE)
+          setDT(long)[, .N, by = .(tree, nodeid, variable, value)]
         }
       }
     },
+    # Sample new data from generative random forest
     sample = function(n) {
       # Sample new observations and get their terminal nodes
       # nodeids dims: [new obs, tree]
-      nodeids <- apply(private$probs, 2, function(x) {
+      nodeids <- apply(private$node_probs, 2, function(x) {
         sample(length(x), n, replace = TRUE, prob = x)
       })
 
       # Randomly select tree for each new obs. (mixture distribution with equal prob.)
       sampled_trees <- sample(private$num_trees, n, replace = TRUE)
       sampled_nodes <- sapply(1:n, function(i) {
-        as.character(nodeids[i, sampled_trees[i]])
+        nodeids[i, sampled_trees[i]]
       })
+      sampled_trees_nodes <- data.table(obs = 1:n, tree = sampled_trees, nodeid = sampled_nodes)
 
       # Get distributions parameters for each new obs.
       if (any(!private$factor_cols)) {
-        obs_params <- sapply(1:n, function(i) {
-          private$params[[sampled_trees[i]]][[sampled_nodes[i]]]
-        }, simplify = "array")
+        obs_params <- merge(sampled_trees_nodes, private$params, by = c("tree", "nodeid"), sort = FALSE, allow.cartesian = TRUE)
+      }
+
+      # Get probabilities for each new obs.
+      if (any(private$factor_cols)) {
+        obs_probs <- merge(sampled_trees_nodes, private$class_probs, by = c("tree", "nodeid"), sort = FALSE, allow.cartesian = TRUE)
       }
 
       # Sample new data from mixture distribution over trees
@@ -129,25 +114,21 @@ genrf <- R6::R6Class(
 
         if (private$factor_cols[j]) {
           # Factor columns: Multinomial distribution
-          draws <- sapply(1:n, function(i) {
-            probs <- private$class_probs[[sampled_trees[i]]][[sampled_nodes[i]]][[colname]]
-            sample.int(n = length(probs), size = 1, prob = probs)
-          })
-          factor(private$levels[[j]][draws])
+          obs_probs[variable == colname, sample(value, 1, prob = N), by = obs]$V1
         } else {
           # Continuous columns: Match estimated distribution parameters with r...() function
           if (private$dist == "normal") {
-            rnorm(n = n, mean = obs_params["mean", colname, ],
-                  sd = obs_params["sd", colname, ])
+            rnorm(n = n, mean = obs_params[variable == colname, mean],
+                  sd = obs_params[variable == colname, sd])
           } else if (private$dist == "exponential") {
-            rexp(n = n, obs_params[colname, ])
+            rexp(n = n, obs_params[variable == colname, rate])
           } else if (private$dist == "geometric") {
-            rgeom(n = n, obs_params[colname, ])
+            rgeom(n = n, obs_params[variable == colname, prob])
           } else if (private$dist %in% c("log-normal", "lognormal")) {
-            rlnorm(n = n, meanlog = obs_params["meanlog", colname, ],
-                   sdlog = obs_params["sdlog", colname, ])
+            rlnorm(n = n, meanlog = obs_params[variable == colname, meanlog],
+                   sdlog = obs_params[variable == colname, sdlog])
           } else if (private$dist == "Poisson") {
-            rpois(n = n, obs_params[colname, ])
+            rpois(n = n, obs_params[variable == colname, lambda])
           } else {
             stop("Unknown distribution.")
           }
@@ -156,10 +137,10 @@ genrf <- R6::R6Class(
 
       # Convert chars and logicals back
       if (any(private$idx_char)) {
-        data_new[, private$idx_char] <- as.data.frame(lapply(data_new[, private$idx_char], as.character))
+        data_new[, private$idx_char] <- as.data.frame(lapply(data_new[, private$idx_char, drop = FALSE], as.character))
       }
       if (any(private$idx_logical)) {
-        data_new[, private$idx_logical] <- as.data.frame(lapply(data_new[, private$idx_logical], function(x) {x == "TRUE"}))
+        data_new[, private$idx_logical] <- as.data.frame(lapply(data_new[, private$idx_logical, drop = FALSE], function(x) {x == "TRUE"}))
       }
 
       # Use original column names
@@ -170,16 +151,15 @@ genrf <- R6::R6Class(
     }
   ),
   private = list(
-    probs = matrix(),
-    params = list(),
-    class_probs = list(),
-    num_trees = integer(),
-    p = integer(),
-    factor_cols = logical(),
-    orig_colnames = character(),
-    levels = list(),
-    idx_char = logical(),
-    idx_logical = logical(),
-    dist = character()
+    node_probs = matrix(), # Selection probabilities for terminal nodes; dims: [nodeid, tree]
+    params = data.table(), # Distribution parameters for numeric columns; columns:
+    class_probs = data.table(), # Probabilities for categorical columns; columns:
+    num_trees = integer(), # Number of trees in the random forest
+    p = integer(), # Number of columns in the data
+    factor_cols = logical(), # Which columns are factors after transforming chars and logicals?
+    orig_colnames = character(), # Original column names of the input data
+    idx_char = logical(), # Which columns are chars before transformations?
+    idx_logical = logical(), # Which columns are logicals before transformations?
+    dist = character() # Distribution for numeric columns
   )
 )
