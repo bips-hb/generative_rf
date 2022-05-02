1460e0a93da20d912ab2c10a986de71c943d2f31
Kristin Blesch <kristin.blesch@web.de>
2022-05-02 14:12:30 +0200

    CTGAN_cpu results

--- benchmark/CTGAN_cpu.py
+++ benchmark/CTGAN_cpu.py
@@ -7,19 +7,19 @@ exec(open("cpu_gpu.py").read())
 
 from sdv.tabular import CTGAN
 
-sub_0 = run_CTGAN_cpu_sub(i = 0,  synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
-sub_0.to_csv("CTGAN_cpu-0.csv")
+sub_0 = run_CTGAN_cpu_sub(range_i = range(8),  synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
+pd.concat(sub_0).to_csv("CTGAN_cpu-0.csv")
 
-sub_1 = run_CTGAN_cpu_sub(i = 1, synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
-sub_1.to_csv("CTGAN_cpu-1.csv")
+sub_1 = run_CTGAN_cpu_sub(range_i = range(9,16), synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
+pd.concat(sub_1).to_csv("CTGAN_cpu-1.csv")
 
-sub_2 = run_CTGAN_cpu_sub(i = 2, synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
-sub_2.to_csv("CTGAN_cpu-2.csv")
+sub_2 = run_CTGAN_cpu_sub(range_i = range(17,24), synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
+pd.concat(sub_2).to_csv("CTGAN_cpu-2.csv")
 
-sub_3 = run_CTGAN_cpu_sub(i = 3, synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
-sub_3.to_csv("CTGAN_cpu-3.csv")
+sub_3 = run_CTGAN_cpu_sub(range_i = range(25, 32), synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
+pd.concat(sub_3).to_csv("CTGAN_cpu-3.csv")
 
-sub_4 = run_CTGAN_cpu_sub(i = 4, synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
-sub_4.to_csv("CTGAN_cpu-4.csv")
+sub_4 = run_CTGAN_cpu_sub(range_i = range(33,40), synthesizer_dict =  {"CTGAN": CTGAN(cuda=False)} )
+pd.concat(sub_4).to_csv("CTGAN_cpu-4.csv")
 
 
--- benchmark/cpu_gpu.py
+++ benchmark/cpu_gpu.py
@@ -86,12 +86,12 @@ def run_sub(synthesizer_dict, R_seed = False):
     metric = sub_metrics, synthesizer =  synthesizer_dict)  for i in range(len(subs))]
     return res
   
-def run_CTGAN_cpu_sub(i, synthesizer_dict):
+def run_CTGAN_cpu_sub(range_i, synthesizer_dict):
     np.random.seed(2022)
     torch.manual_seed(2022)
-    res = scores(data_train = train_sub[i], data_test = test_sub[i], list_of_classifiers = sub_classifiers, 
-    metric = sub_metrics, synthesizer =  synthesizer_dict) 
-    return res
+    res = (scores(data_train = train_sub[i], data_test = test_sub[i], list_of_classifiers = sub_classifiers, 
+    metric = sub_metrics, synthesizer =  synthesizer_dict) for i in range_i)
+    return list(res)
 
 
 
