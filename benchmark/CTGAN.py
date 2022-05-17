28ee64bd6e410d9c19ff543a056b74f71ecb6450
kristinblesch <48204979+kristinblesch@users.noreply.github.com>
2022-05-17 13:01:39 +0200

    uncomment benchmark

--- benchmark/CTGAN.py
+++ benchmark/CTGAN.py
@@ -8,14 +8,14 @@ from sdv.tabular import CTGAN
 
 
 # adult
-#adult_res = run_benchmark(training_data=adult_train, test_data=adult_test, classifiers=adult_classifiers,
-#metrics= adult_metrics, data_synthesizer=  {"CTGAN": CTGAN()})
-#pd.concat(adult_res).to_csv("CTGAN_adult.csv")
+adult_res = run_benchmark(training_data=adult_train, test_data=adult_test, classifiers=adult_classifiers,
+metrics= adult_metrics, data_synthesizer=  {"CTGAN": CTGAN()})
+pd.concat(adult_res).to_csv("CTGAN_adult.csv")
 
 # census
-#census_res = run_benchmark(training_data = census_train, test_data = census_test, classifiers = census_classifiers,
-#metrics = census_metrics, data_synthesizer = {"CTGAN": CTGAN()})
-#pd.concat(census_res).to_csv("CTGAN_census.csv")
+census_res = run_benchmark(training_data = census_train, test_data = census_test, classifiers = census_classifiers,
+metrics = census_metrics, data_synthesizer = {"CTGAN": CTGAN()})
+pd.concat(census_res).to_csv("CTGAN_census.csv")
 
 # covtype
 covtype_res = run_benchmark(training_data = covtype_train, test_data= covtype_test, classifiers= covtype_classifiers,
@@ -23,9 +23,9 @@ metrics= covtype_metrics, data_synthesizer= {"CTGAN": CTGAN()})
 pd.concat(covtype_res).to_csv("CTGAN_covtype.csv")
 
 # credit
-#credit_res = run_benchmark(training_data= credit_train, test_data= credit_test, classifiers= credit_classifiers,
-#metrics= credit_metrics, data_synthesizer= {"CTGAN": CTGAN()})
-#pd.concat(credit_res).to_csv("CTGAN_credit.csv")
+credit_res = run_benchmark(training_data= credit_train, test_data= credit_test, classifiers= credit_classifiers,
+metrics= credit_metrics, data_synthesizer= {"CTGAN": CTGAN()})
+pd.concat(credit_res).to_csv("CTGAN_credit.csv")
 
 # intrusion
 intrusion_res = run_benchmark(training_data= intrusion_train, test_data= intrusion_test, classifiers= intrusion_classifiers,
