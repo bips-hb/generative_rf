7a0d1752bd53fb736a3834193f9c20b4938e6589
Marvin Wright <github@wrig.de>
2022-07-30 00:46:15 +0200

    add comments to loglik function

--- nfs/myloglik.R
+++ nfs/myloglik.R
@@ -4,37 +4,36 @@ myloglik <- function(mod, dat) {
   node_probs <- mod$.__enclos_env__$private$node_probs
   td_long <- melt(data.table(obs = 1:nrow(dat), dat), id.vars = "obs")
   
+  # Terminal node predictions
   pred <- predict(rf, dat, type = "terminalNodes")
   preds <- rbindlist(lapply(1:ncol(pred$predictions), function(i) {
     data.table(tree = i, obs = 1:nrow(pred$predictions), nodeid = pred$predictions[, i])
   }))
   
+  # Coverage values
   np <- rbindlist(lapply(1:ncol(node_probs), function(i) {
     data.table(tree = i, nodeid = 1:nrow(node_probs), prob = node_probs[, i])
   }))
   
-  #aa <- merge(params, np, by = c("tree", "nodeid"))
+  # Merge parameters with predicted nodes, data values and estimate density in nodes
   aa <- merge(params[!is.na(sd), ], np, by = c("tree", "nodeid"))
   bb <- merge(aa, preds, by = c("tree", "nodeid"), allow.cartesian = TRUE)
   cc <- merge(bb, td_long, by = c("obs", "variable"))
   cc[, lik := dnorm(value, mean = mean, sd = sd)]
-  
-  #cc[, lik := max(lik, 1e-100)]
   
   # Normalization constant
   zz <- unique(cc[, .(tree, nodeid, prob)])
   nc <- zz[, sum(prob), by = tree]
   colnames(nc)[2] <- "normconst"
   
+  # Product of feature densities, weighted by coverage
   dd <- cc[, prod(lik), by = .(obs, tree, nodeid, prob)]
-  
   ee <- merge(dd, nc, by = "tree")
-  
   ee[, lik := prob/normconst * V1]
-  #ee[, lik := prob * V1]
-  #ee[, lik := V1]
-  
+
+  # Sum over nodes
   ff <- ee[, sum(lik), by = .(obs, tree)]
   
-  (ff[, log(mean(V1)), by = obs]$V1)
+  # Average over trees
+  ff[, log(mean(V1)), by = obs]$V1
 }
