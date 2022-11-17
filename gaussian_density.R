d5428e85167cde3a6c9705902693f9faaa52550a
David Watson <david.s.watson11@gmail.com>
2022-11-17 09:21:04 +0000

    plotfix

--- gaussian_density.R
+++ gaussian_density.R
@@ -22,7 +22,7 @@ ll_mle <- mvtnorm::dmvnorm(tst, mean = est$mu, sigma = est$sigma, log = TRUE)
 -mean(ll_mle)
 
 # Adversarial RF
-arf <- adversarial_rf(trn, num_trees = 10, min_node_size = 5, delta = 0)
+arf <- adversarial_rf(trn, num_trees = 100, mtry = 3)
 fd <- forde(arf, x_trn = trn, x_tst = tst)
 ll_arf <- fd$loglik
 -mean(ll_arf)
@@ -32,9 +32,11 @@ table(is.finite(ll_arf))
 
 # Plot
 library(ggplot2)
-df <- data.frame('MLE' = ll_mle, 'ARF' = ll_arf)
+df <- data.frame('MLE' = -ll_mle, 'ARF' = -ll_arf)
 ggplot(df, aes(MLE, ARF)) + 
   geom_point() +
   geom_abline(slope = 1, intercept = 0, linetype = 'dashed', color = 'red') + 
+  scale_x_log10() + 
+  scale_y_log10() + 
   theme_bw()
 
