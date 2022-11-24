b699a6891bdb550df6e6daae6f60b7e894dc3e97
Kristin Blesch <kristin.blesch@web.de>
2022-11-24 18:21:37 +0100

    CTABGAN all data sets

--- benchmark/CTABGAN.py
+++ benchmark/CTABGAN.py
@@ -17,20 +17,20 @@ except:
 exec(open("benchmark_individual.py").read())
 
 
+# prepare CTABGAN+ model
+def ctabgan_fun(real_data):
+    # find categorical columns
+    cat_col = real_data.select_dtypes('object').columns.to_list()
+    # find integer columns
+    int_col = real_data.select_dtypes('int').columns.to_list()
+    return CTABGAN( df = real_data,
+    test_ratio = 0.2,
+    categorical_columns = cat_col,
+    integer_columns = int_col,
+    problem_type = { None: None}
+    )
 
-# TO DO - automize detection of categorical variables and pass list in arguments
-def ctabgan_fun(real_data): 
-    return CTABGAN(df = real_data,
-                 test_ratio = 0.20,
-                 categorical_columns = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'label'], 
-                 log_columns = [],
-                 mixed_columns= {'capital-loss':[0.0],'capital-gain':[0.0]},
-                 general_columns = ["age"],
-                 non_categorical_columns = [],
-                 integer_columns = ['age', 'fnlwgt','capital-gain', 'capital-loss','hours-per-week'],
-                 problem_type= {"Classification": 'label'})
-
-
+# define synthetic data generation
 def synth_data(data_train, synthesizer):
     """
     Arguments: 
@@ -45,6 +45,44 @@ def synth_data(data_train, synthesizer):
     else: 
         print("please use CTABGAN synthesizer")
 
-adult_res = run_benchmark(training_data= adult_train, test_data = adult_test, classifiers= adult_classifiers, 
-metrics= adult_metrics, data_synthesizer= {"CTABGAN+": ctabgan_fun})
-pd.concat(adult_res).to_csv("CTABGAN_adult.csv")
+# adult
+#adult_res = run_benchmark(training_data= adult_train, test_data = adult_test, classifiers= adult_classifiers, 
+#metrics= adult_metrics, data_synthesizer= {"CTABGAN+": ctabgan_fun})
+#pd.concat(adult_res).to_csv("CTABGAN_adult.csv")
+
+# census
+census_res = run_benchmark(training_data = census_train, test_data = census_test, classifiers = census_classifiers,
+metrics = census_metrics, data_synthesizer = {"CTABGAN+": ctabgan_fun})
+pd.concat(census_res).to_csv("CTABGAN_census.csv")
+
+# covtype
+covtype_res = run_benchmark(training_data = covtype_train, test_data= covtype_test, classifiers= covtype_classifiers,
+metrics= covtype_metrics, data_synthesizer= {"CTABGAN+": ctabgan_fun})
+pd.concat(covtype_res).to_csv("CTABGAN_covtype.csv")
+
+# credit
+credit_res = run_benchmark(training_data= credit_train, test_data= credit_test, classifiers= credit_classifiers,
+metrics= credit_metrics, data_synthesizer= {"CTABGAN+": ctabgan_fun})
+pd.concat(credit_res).to_csv("CTABGAN_credit.csv")
+
+# intrusion
+#intrusion_res = run_benchmark(training_data= intrusion_train, test_data= intrusion_test, classifiers= intrusion_classifiers,
+#metrics= intrusion_metrics, data_synthesizer= {"CTABGAN+": ctabgan_fun})
+#pd.concat(intrusion_res).to_csv("CTABGAN_intrusion.csv")
+
+#-----------------
+## some notes
+# --------------
+# important note: to sample data points, CTABGAN needs at least 2 different levels, 
+# otherwise sampling never finishes -- take this into account when subsampling!
+#syn = ctabgan_fun(real_data=dd)
+#syn.fit()
+#syn.generate_samples()
+
+#dd =intrusion_train[1].iloc[1:500,1:7]
+#dd.select_dtypes('object').columns.to_list()
+#dd.select_dtypes('int').columns.to_list()
+
+#ll =[]
+#for i in rep: ll.append(mnist12_test[i].describe()) 
+#ll = pd.concat(ll)
