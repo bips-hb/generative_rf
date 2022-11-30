ae627f4a33b551ffde6fbcd6f025a6d7a0a55dab
Kristin Blesch <kristin.blesch@web.de>
2022-11-30 10:03:20 +0100

    add rebuttal results

--- benchmark/superscript.sh
+++ benchmark/superscript.sh
@@ -4,12 +4,13 @@
 #python CTGAN_gpu.py
 #python CTGAN_cpu.py
 source /home/blesch/miniconda3/etc/profile.d/conda.sh
-conda activate ctabgan
 conda info
-python CTABGAN.py 
-conda activate gan
-conda info
-python RCCGAN.py
+#conda activate ctabgan
+#conda info
+#python CTABGAN.py 
+#conda activate gan
+#conda info
+#python RCCGAN.py
 conda activate itgan
 conda info
 python ITGAN_adult.py
