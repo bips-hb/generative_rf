f8d65feb306dcceaf51c471b3181b606e178238c
David Watson <david.s.watson11@gmail.com>
2022-12-04 00:21:22 +0000

    n, d

--- experiments/NLL_binary.R
+++ experiments/NLL_binary.R
@@ -11,7 +11,8 @@ registerDoMC(10)
 set.seed(123, "L'Ecuyer-CMRG")
 
 # Output file
-df <- data.table('dataset' = NA_character_, 'NLL' = NA_real_)
+df <- data.table('dataset' = NA_character_, 'n' = NA_real_, 'd' = NA_real_, 
+                 'NLL' = NA_real_)
 saveRDS(df, './experiments/NLL_binary.rds')
 
 # Likelihood evaluation function
@@ -20,7 +21,9 @@ nll_fn <- function(dataset) {
   trn <- fread(paste0('./data/', dataset, '/', dataset, '.train.data'))
   val <- fread(paste0('./data/', dataset, '/', dataset, '.valid.data'))
   tst <- fread(paste0('./data/', dataset, '/', dataset, '.test.data'))
-  colnames(trn) <- colnames(val) <- colnames(tst) <- paste0('x', 1:ncol(trn))
+  n <- nrow(trn)
+  d <- ncol(trn)
+  colnames(trn) <- colnames(val) <- colnames(tst) <- paste0('x', 1:d)
   # Train
   suppressWarnings(
     arf <- adversarial_rf(trn, num_trees = 100, max_iters = 2, verbose = FALSE)
@@ -29,7 +32,8 @@ nll_fn <- function(dataset) {
     fd <- forde(arf, x_trn = val, x_tst = tst, batch = 5000)
   )
   # Export
-  out <- data.table('dataset' = dataset, 'NLL' = -mean(fd$loglik))
+  out <- data.table('dataset' = dataset, 'n' = n, 'd' = d, 
+                    'NLL' = -mean(fd$loglik))
   df <- readRDS('./experiments/NLL_binary.rds')
   df <- rbind(df, out)
   saveRDS(df, './experiments/NLL_binary.rds')
