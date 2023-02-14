2c7311d9f4a1ee4c94f9a92fff1165f4ec396079
krisbl <kristin.blesch@web.de>
2023-02-14 20:48:31 +0000

    update plots

--- benchmark/runtime/plot.R
+++ benchmark/runtime/plot.R
@@ -38,15 +38,16 @@ df$`model and processing unit` = paste0(df$model, " ", "(", df$processing_unit, 
 # 1. wall time
 #-------------------
 
-colcol = c("#BC3c29FF", "#F39B7FFF","#0072B5FF",  "#6F99ADEF","#E18727FF", "#FEDC91FF", "#20854EFF")
+colcol = c("#BC3c29FF", "#BC3c29FF","#0072B5FF",  "#0072B5FF","#E18727FF", "#E18727FF", "#20854EFF")
 #--------------------
 # training
 #--------------------
-plt_samplesize_train = ggplot(data = df , aes(x = n, y = mean_wall_time_train, color = `model and processing unit`)) + geom_line()+
+plt_samplesize_train = ggplot(data = df , aes(x = n, y = mean_wall_time_train, color = `model and processing unit`)) + 
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_wall_time_train-sd_wall_time_train, ymax=mean_wall_time_train+sd_wall_time_train, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+ 
   geom_point(aes(shape= `model and processing unit`) )+
-  # scale_x_continuous(trans='log10')+
+  scale_x_continuous(trans='log10')+
   scale_y_continuous(trans= 'log10')+
   ylab("Time (sec)")+
   theme_bw()+
@@ -54,27 +55,28 @@ plt_samplesize_train = ggplot(data = df , aes(x = n, y = mean_wall_time_train, c
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Sample size")+
   ggtitle("Training")
 plt_samplesize_train
 #--------------------
 # sampling
 #--------------------
-plt_samplesize_sample = ggplot(data = df , aes(x = n, y = mean_wall_time_sample, color = `model and processing unit`)) + geom_line()+
+plt_samplesize_sample = ggplot(data = df , aes(x = n, y = mean_wall_time_sample, color = `model and processing unit`)) + 
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_wall_time_sample-sd_wall_time_sample, ymax=mean_wall_time_sample+sd_wall_time_sample, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+ 
   geom_point(aes(shape= `model and processing unit`) )+
-  # scale_x_continuous(trans='log10')+
-  scale_y_continuous(trans= 'log10')+
+  scale_x_continuous(trans='log10')+
+  scale_y_continuous(trans= 'log10', labels = scales::label_number(accuracy = 1))+
   ylab("")+
   theme_bw()+
-  #theme(legend.key = element_rect(fill = "white"), legend.text = element_text(color = "white"), legend.title = element_text(color = "white")) +
-  #guides(color = guide_legend(override.aes = list(color = NA, fill = NA)))+
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Sample size")+
   ggtitle("Sampling")
 
@@ -86,11 +88,12 @@ plt_samplesize_sample = ggplot(data = df , aes(x = n, y = mean_wall_time_sample,
 # training
 #--------------------
 
-plt_samplesize_process_time_train <- ggplot(data = df, aes(x = n, y = mean_process_time_train, color = `model and processing unit`)) + geom_line()+
+plt_samplesize_process_time_train <- ggplot(data = df, aes(x = n, y = mean_process_time_train, color = `model and processing unit`)) + 
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_process_time_train-sd_process_time_train, ymax=mean_process_time_train+sd_process_time_train, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+
   geom_point(aes(shape= `model and processing unit`) )+
-  #scale_x_continuous(trans='log10')+
+  scale_x_continuous(trans='log10')+
   scale_y_continuous(trans= 'log10')+
   ylab("Process time (sec)")+
   theme_bw()+
@@ -98,7 +101,8 @@ plt_samplesize_process_time_train <- ggplot(data = df, aes(x = n, y = mean_proce
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Sample size")+
   ggtitle("Training")
 
@@ -106,12 +110,13 @@ plt_samplesize_process_time_train <- ggplot(data = df, aes(x = n, y = mean_proce
 # sampling
 #--------------------
 
-plt_samplesize_process_time_sample <- ggplot(data = df, aes(x = n, y = mean_process_time_sample, color = `model and processing unit`)) + geom_line()+
+plt_samplesize_process_time_sample <- ggplot(data = df, aes(x = n, y = mean_process_time_sample, color = `model and processing unit`)) +
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_process_time_sample-sd_process_time_sample, ymax=mean_process_time_sample+sd_process_time_sample, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+
   geom_point(aes(shape= `model and processing unit`) )+
-  #scale_x_continuous(trans='log10')+
-  scale_y_continuous(trans= 'log10')+
+  scale_x_continuous(trans='log10')+
+  scale_y_continuous(trans= 'log10', labels = scales::label_number(accuracy = 1))+
   ylab("")+
   theme_bw()+
   #theme(legend.key = element_rect(fill = "white"), legend.text = element_text(color = "white"), legend.title = element_text(color = "white")) +
@@ -119,7 +124,8 @@ plt_samplesize_process_time_sample <- ggplot(data = df, aes(x = n, y = mean_proc
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Sample size")+
   ggtitle("Sampling")
 
@@ -157,12 +163,13 @@ df$`model and processing unit` = paste0(df$model, " ", "(", df$processing_unit, 
 # time plots dimensionality
 # 1. wall time
 #-------------------
-colcol = c("#BC3c29FF", "#F39B7FFF","#0072B5FF",  "#6F99ADEF","#E18727FF", "#FEDC91FF", "#20854EFF")
+
 #-------------------
 # training
 #-------------------
 
-plt_dimensionality_train = ggplot(data = df, aes(x = d, y = mean_wall_time_train, color = `model and processing unit`)) + geom_line()+
+plt_dimensionality_train = ggplot(data = df, aes(x = d, y = mean_wall_time_train, color = `model and processing unit`)) + 
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_wall_time_train-sd_wall_time_train, ymax=mean_wall_time_train+sd_wall_time_train, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+
   geom_point(aes(shape= `model and processing unit`) )+
@@ -175,7 +182,8 @@ plt_dimensionality_train = ggplot(data = df, aes(x = d, y = mean_wall_time_train
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Dimensionality")+
   ggtitle("Training")
 plt_dimensionality_train
@@ -183,12 +191,13 @@ plt_dimensionality_train
 # sampling
 #-------------------
 
-plt_dimensionality_sample = ggplot(data = df, aes(x = d, y = mean_wall_time_sample, color = `model and processing unit`)) + geom_line()+
+plt_dimensionality_sample = ggplot(data = df, aes(x = d, y = mean_wall_time_sample, color = `model and processing unit`)) + 
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_wall_time_sample-sd_wall_time_sample, ymax=mean_wall_time_sample+sd_wall_time_sample, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+
   geom_point(aes(shape= `model and processing unit`) )+
-  scale_x_continuous(breaks = seq(2,14,2), minor_breaks = c() )+
-  scale_y_continuous(trans= 'log10')+
+#  scale_x_continuous(breaks = seq(2,14,2), minor_breaks = c() )+
+  scale_y_continuous(trans= 'log10', labels = scales::label_number(accuracy = 1))+
   #scale_color_npg()+
   ylab(" ")+
   theme_bw()+
@@ -196,7 +205,8 @@ plt_dimensionality_sample = ggplot(data = df, aes(x = d, y = mean_wall_time_samp
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Dimensionality")+
   ggtitle("Sampling")
 
@@ -208,7 +218,8 @@ plt_dimensionality_sample = ggplot(data = df, aes(x = d, y = mean_wall_time_samp
 #-------------------
 # training
 #-------------------
-plt_dimensionality_process_time_train <- ggplot(data = df, aes(x = d, y = mean_process_time_train, color = `model and processing unit`)) + geom_line()+
+plt_dimensionality_process_time_train <- ggplot(data = df, aes(x = d, y = mean_process_time_train, color = `model and processing unit`)) + 
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_process_time_train-sd_process_time_train, ymax=mean_process_time_train+sd_process_time_train, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+
   geom_point(aes(shape= `model and processing unit`) )+
@@ -220,14 +231,16 @@ plt_dimensionality_process_time_train <- ggplot(data = df, aes(x = d, y = mean_p
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Dimensionality")+
   ggtitle("Training")
 
 #-------------------
 # sampling
 #-------------------
-plt_dimensionality_process_time_sample <- ggplot(data = df, aes(x = d, y = mean_process_time_sample, color = `model and processing unit`)) + geom_line()+
+plt_dimensionality_process_time_sample <- ggplot(data = df, aes(x = d, y = mean_process_time_sample, color = `model and processing unit`)) + 
+  geom_line(aes(lty = `model and processing unit`))+
   geom_ribbon(aes(ymin=mean_process_time_sample-sd_process_time_sample, ymax=mean_process_time_sample+sd_process_time_sample, fill = `model and processing unit`), 
               alpha=0.2, lty = "blank")+
   geom_point(aes(shape= `model and processing unit`) )+
@@ -239,7 +252,8 @@ plt_dimensionality_process_time_sample <- ggplot(data = df, aes(x = d, y = mean_
   scale_discrete_manual(values = colcol ,aesthetics = c("colour", "fill"), 
                         breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)','CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'),
                         name = "Method")+
-  scale_shape_manual(values = c(1, 16, 0, 15, 2, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_shape_manual(values = c(16, 16, 15, 15, 17, 17,8), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
+  scale_linetype_manual(values = c(1, 3, 1, 3, 1, 3,1), name = "Method",breaks=c('CTABGAN (CPU)', 'CTABGAN (GPU)', 'CTGAN (CPU)', 'CTGAN (GPU)', 'TVAE (CPU)', 'TVAE (GPU)', 'FORGE (CPU)'))+
   xlab("Dimensionality")+
   ggtitle("Sampling")
 
@@ -255,21 +269,23 @@ p1 <- plot_grid(plt_samplesize_train + theme(legend.position = "none"),
           plt_samplesize_sample + theme(legend.position = "none"), 
           plt_dimensionality_train + theme(legend.position = "none"), 
           plt_dimensionality_sample + theme(legend.position = "none"), 
-          ncol = 2, rel_widths = c(.53, .47) ,rel_heights = c(.5, .5), labels = "AUTO", label_x = c(.06, 0))
+         # ncol = 2, rel_widths = c(.53, .47) ,rel_heights = c(.5, .5), labels = "AUTO", label_x = c(.06, 0))
+          ncol = 2, rel_widths = c(.5, .5) ,rel_heights = c(.5, .5), labels = "AUTO", label_x = c(.06, 0))
 legend1 <- get_legend(
-  plt_dimensionality_sample + theme(legend.position = "bottom")
+  plt_samplesize_train + theme(legend.position = "bottom")
 )
-plot_grid(p1, legend1, ncol = 1, rel_heights = c(1, .1))
-ggsave("time.pdf", width = 8, height = 4)
+plot_grid(p1, legend1, ncol = 1, rel_heights = c(0.9, .1))
+ggsave("time.pdf", width = 10, height = 5.7)
 # bump in adult at dimensionality = 14 (~3 sec until d = 13, ~6 sec at d >= 14)
 p2 <- plot_grid(plt_samplesize_process_time_train + theme(legend.position = "none"),
           plt_samplesize_process_time_sample + theme(legend.position = "none"), 
           plt_dimensionality_process_time_train + theme(legend.position = "none"), 
           plt_dimensionality_process_time_sample + theme(legend.position = "none"), 
-          ncol = 2, rel_widths = c(.513, .487), rel_heights = c(.5, .5), labels = "AUTO", label_x = c(.06, 0))
+         # ncol = 2, rel_widths = c(.513, .487), rel_heights = c(.5, .5), labels = "AUTO", label_x = c(.06, 0))
+          ncol = 2, rel_widths = c(.5, .5), rel_heights = c(.5, .5), labels = "AUTO", label_x = c(.06, 0))
 legend2 <- get_legend(
-  plt_dimensionality_process_time_sample + theme(legend.position = "bottom")
+  plt_samplesize_train + theme(legend.position = "bottom")
 )
-plot_grid(p2, legend2, ncol = 1, rel_heights = c(1, .1))
+plot_grid(p2, legend2, ncol = 1, rel_heights = c(0.9, .1))
 ggsave("process_time2.pdf", width = 10, height = 5.7)
 
